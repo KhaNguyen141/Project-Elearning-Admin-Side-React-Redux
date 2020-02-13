fbbb4b8957455d85c8bb53ed18de9553deb2e8c7
unknown <khanguyen461995@gmail.com>
2020-02-13 14:57:05 +0700

    rotate device function

--- src/App.js
+++ src/App.js
@@ -21,14 +21,92 @@ import SidebarButton from "./Components/SidebarButton/SidebarButton";
 import SeperatorLine from "./Components/SeperatorLine/SeperatorLine";
 import PageNotFound from "./Screens/PageNotFound/PageNotFound";
 
+import ScreenRotationIcon from '@material-ui/icons/ScreenRotation';
+
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      sideDrawerOpen: false
+      sideDrawerOpen: false,
+      isMobileRotate: false
 
     };
+    this.updatePredicate = this.updatePredicate.bind(this);
+  }
+  
+  render() {
+    const notFoundPage = () => (
+      <PageNotFound />
+    )
+
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler}/>;
+    }
+
+    const isMobileRotate = this.state.isMobileRotate;
+    
+    return (
+      <BrowserRouter> 
+        <div>
+          {isMobileRotate ? (
+            <Switch>
+
+              <Route exact path="/admin" component={HomeScreen} />
+              <Route exact path="/admin/detail" component={ProfileDetail} />
+              <Route exact path="/admin/list-user-manage" component={UserManagementsComponent} />
+              <Route exact path="/admin/list-course-manage" component={CourseManagementsComponent} />
+              <Route exact path="/admin/course-manage" component={AddCourseComponent} />
+              <Route exact path="/admin/user-manage" component={AddUserComponent} />
+              <Route component={notFoundPage} />
+              <Route path="/" component={HomeScreen} />
+            </Switch>
+          ) : (
+              <div className="screenRotationContainer">
+                <ScreenRotationIcon />
+                <p>Please rotate your device</p>
+              </div>
+            )}
+        </div>
+
+        {this.props.credentials &&
+          <div>
+            {backdrop}
+            <NavigationBarComponent show={this.state.sideDrawerOpen}/>
+
+            <SidebarButton click={this.drawerToggleClickHandler}/>
+
+            <SeperatorLine />
+
+          </div>
+      
+        }
+        
+      </BrowserRouter>
+    );
+  }
+  componentDidMount() {
+    document.body.style.background = "#c49e00";
+    this.updatePredicate();
+    window.addEventListener("resize", this.updatePredicate);
+    
+    const adminLoginStr = localStorage.getItem("adminLogin");
+    const adminAccessToken = localStorage.getItem('accessToken');
+    if (adminLoginStr && adminAccessToken) {
+      restConnector.defaults.headers['Authorization'] = "Bearer " + adminAccessToken
+      this.props.dispatch( reduxAction( ADMIN_LOGIN, JSON.parse(adminLoginStr) )  );
+
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updatePredicate);
+  }
+
+  updatePredicate() {
+    this.setState({ isMobileRotate: window.innerWidth > 560 });
   }
 
   drawerToggleClickHandler = () => {
@@ -43,60 +121,6 @@ class App extends Component {
   
     });
   };
-  
-  render() {
-    const notFoundPage = () => (
-      <PageNotFound />
-    )
-
-    let backdrop;
-
-    if (this.state.sideDrawerOpen) {
-      backdrop = <Backdrop click={this.backdropClickHandler}/>;
-    }
-    
-    return (
-      <BrowserRouter> 
-      
-        {this.props.credentials &&
-          <div>
-            {backdrop}
-            <NavigationBarComponent show={this.state.sideDrawerOpen}/>
-
-            <SidebarButton click={this.drawerToggleClickHandler}/>
-
-            <SeperatorLine />
-
-          </div>
-      
-        }
-      
-        <Switch>
-        
-            <Route exact path="/admin" component={HomeScreen} />
-            <Route exact path="/admin/detail" component={ProfileDetail} />
-            <Route exact path="/admin/list-user-manage" component={UserManagementsComponent} />
-            <Route exact path="/admin/list-course-manage" component={CourseManagementsComponent} />
-            <Route exact path="/admin/course-manage" component={AddCourseComponent} />
-            <Route exact path="/admin/user-manage" component={AddUserComponent} />
-            <Route component={notFoundPage} />
-            <Route path="/" component={HomeScreen} />
-        </Switch>
-        
-      </BrowserRouter>
-    );
-  }
-  componentDidMount() {
-    document.body.style.background = "#c49e00";
-    
-    const adminLoginStr = localStorage.getItem("adminLogin");
-    const adminAccessToken = localStorage.getItem('accessToken');
-    if (adminLoginStr && adminAccessToken) {
-      restConnector.defaults.headers['Authorization'] = "Bearer " + adminAccessToken
-      this.props.dispatch( reduxAction( ADMIN_LOGIN, JSON.parse(adminLoginStr) )  );
-
-    }
-  }
 }
 
 const mapStateToProps = (state) => ({
